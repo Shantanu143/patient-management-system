36c516c266d96a3805ea5cb9989ce9af946a3c44
Shantanu143 <shantanunirapal@gmail.com>
2025-01-10 12:12:51 +0000

    update api created

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { loginAdmin, registerDoctor } from "../controllers/adminController.js";
+import {
+  loginAdmin,
+  registerDoctor,
+  updateDoctor,
+} from "../controllers/adminController.js";
 import authAdmin from "../middleware/authAdmin.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login-admin", loginAdmin);
 adminRouter.post("/register-doctor", authAdmin, registerDoctor);
+adminRouter.put("/update-doctor", authAdmin, updateDoctor);
 
 export default adminRouter;
