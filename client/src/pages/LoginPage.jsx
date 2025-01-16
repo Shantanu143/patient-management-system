b0287053ad29bf88d03706a05eb143a1eac79329
Omkar <jadhavpatil2999@gmail.com>
2025-01-16 13:54:04 +0530

    login form ux changes

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -66,10 +66,10 @@ const LoginPage = () => {
     <div className="flex justify-center items-center w-full h-screen bg-custom-gradient">
       <form
         onSubmit={submitForm}
-        className="min-w-80 w-2/6 mx-auto border-2 border-blue-300 p-12 rounded-md transition-all duration-300 transform hover:-translate-y-1 hover:shadow-xl hover:shadow-blue-700 "
+        className="min-w-80 w-2/6 mx-auto border-2 border-blue-300 p-10 rounded-md transition-all duration-300 transform hover:-translate-y-1 hover:shadow-xl hover:shadow-blue-700 "
       >
-        <h1 className="text-2xl font-medium text-white pb-5">
-          Login as {role === 'admin' ? 'admin' : 'doctor'}
+        <h1 className="text-2xl font-bold text-white mb-5 text-center ">
+          {role === 'admin' ? 'Admin' : 'Doctor'} Login
         </h1>
 
         <div className="mb-5 ">
@@ -88,12 +88,12 @@ const LoginPage = () => {
             required
           />
         </div>
-        <div className="mb-5">
+        <div className="mb-5 ">
           <label
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-white"
           >
-            Your password
+            Your Password
           </label>
           <input
             type="password"
@@ -104,39 +104,37 @@ const LoginPage = () => {
             placeholder="*********"
           />
         </div>
+
+        <div className="flex items-center w-full justify-center ">
+          <button
+            type="submit"
+            className="relative hover:bg-blue-600 overflow-hidden font-medium rounded-lg text-sm w-full sm:w-auto text-center transition-all border border-blue-500 bg-blue-500 text-white"
+          >
+            <span className="block transition-transform  px-5 py-2.5">
+              Login
+            </span>
+            {/*  */}
+          </button>
+        </div>
         {role === 'admin' ? (
-          <p className="text-white mb-4">
-            Login as doctor{' '}
+          <p className="text-white mt-4 text-center">
             <span
               onClick={() => setRole('doctor')}
-              className="text-white underline underline-offset-2 cursor-pointer hover:text-blue-300"
+              className="text-blue-200 text-sm font-semibold underline capitalize underline-offset-2 cursor-pointer hover:text-blue-500 opacity-40"
             >
-              click here
+              Login as doctor{' '}
             </span>
           </p>
         ) : (
-          <p className="text-white mb-4">
-            Login as admin{' '}
+          <p className="text-white mt-4 text-center">
             <span
               onClick={() => setRole('admin')}
-              className="text-white underline cursor-pointer hover:text-blue-300"
+              className="text-blue-200 text-sm font-semibold underline capitalize underline-offset-2 cursor-pointer hover:text-blue-500 opacity-40"
             >
-              click here
+              Login as admin{' '}
             </span>
           </p>
         )}
-
-        <button
-          type="submit"
-          className="relative group text-white bg-blue-500 overflow-hidden font-medium rounded-lg text-sm w-full sm:w-auto text-center transition-all"
-        >
-          <span className="block transition-transform transform group-hover:-translate-y-full duration-500  px-5 py-2.5">
-            Submit
-          </span>
-          <span className="absolute inset-0 block transition-transform transform translate-y-full group-hover:translate-y-0 duration-500 px-5 py-2.5">
-            Submit
-          </span>
-        </button>
       </form>
     </div>
   );
